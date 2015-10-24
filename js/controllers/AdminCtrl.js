656c80a3bd2b2f908dcaee11668a73edc68936e1
MichaelLSmith <mlsmith45@gmail.com>
2015-10-24 12:14:13 -0400

    styled admin page

--- js/controllers/AdminCtrl.js
+++ js/controllers/AdminCtrl.js
@@ -1,11 +1,18 @@
 app.controller('AdminCtrl',AdminCtrl);
 
 function AdminCtrl(productService,$location,products){
-	var self = this;
+	self = this;
+    this.$location = $location;
 
 	//services
 	this.productService = productService;
 	this.products = products;
 	console.log(this.products);
 
+}
+
+AdminCtrl.prototype.logout = function(){
+    localStorage.removeItem('products');
+    localStorage.removeItem('authToken');
+    self.$location.path('/login');
 }
